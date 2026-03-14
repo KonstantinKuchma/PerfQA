99bee6bdb21fef575b06e1915c0d096323f43f49
KonstantinKuchma <knstntnkuchma@gmail.com>
2026-03-14 10:19:36 +0300

    added homework1

---
+++ src/homework1/homework1_2.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class homework1_2 {
+
+    public static void main(String[] args) {
+
+        int n = 26;
+        int a = n%10;
+        int b = (n/10)%10;
+        int c = a+b;
+
+        System.out.println(c);
+    }
+}
---
+++ src/homework1/homework1_4.java
@@ -0,0 +1,12 @@
+package homework1;
+
+public class homework1_4 {
+
+    public static void main(String[] args) {
+
+        double n = 1.1;
+        int a = (int)(n+0.5);
+
+        System.out.println(a);
+    }
+}
