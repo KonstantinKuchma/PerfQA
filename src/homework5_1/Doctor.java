23beb5dad9fc43ebaa8dc003b29141a9fd218093
KonstantinKuchma <knstntnkuchma@gmail.com>
2026-03-27 18:32:44 +0300

    added homework5_1

---
+++ src/homework5_1/Doctor.java
@@ -0,0 +1,8 @@
+package homework5_1;
+
+public class Doctor { // родительский класс доктор с одним методом лечить
+
+    public void treat() {
+        System.out.println("Доктор лечит");
+    }
+}
---
+++ src/homework5_1/Pathient.java
@@ -0,0 +1,12 @@
+package homework5_1;
+
+public class Pathient {
+
+    Doctor doctor; // задаем врача
+    int code; // вводим переменную кода плана лечения
+
+    public Pathient(int code) { // задаем возможность ввести код плана лечения
+        this.code = code;
+    }
+
+}
